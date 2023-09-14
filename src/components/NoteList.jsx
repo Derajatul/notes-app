b4a9b6f257c0ad11b259d15fffd70f88badfddd2
Derajatul <101638355+Derajatul@users.noreply.github.com>
2023-09-14 00:44:03 +0000

    add NoteList component ✅

---
+++ src/components/NoteList.jsx
@@ -0,0 +1,21 @@
+import styles from './NoteList.module.css'
+import CardNote from './CardNote'
+import { showFormattedDate } from '../utils/index'
+
+const NoteList = ({notes}) => {
+  return(
+    <div className={styles.container}>
+      <h2>Catatan Aktif</h2>
+      <div className={styles.list}>
+      {notes.map(note => (
+        <CardNote 
+          title={note.title} 
+          body={note.body} 
+          createdAt={showFormattedDate(note.createdAt)}
+        />
+      ))}
+      </div>
+    </div>
+  )
+}
+export default NoteList
